23c0d5ddb42a9d389e86351a5ed5bf04fbe4a6f3
netabirami <net.abirami@gmail.com>
2023-09-29 11:25:44 +0200

    first code

---
+++ SimpleProgram.java
@@ -0,0 +1,9 @@
+public class SimpleProgram {
+    public static void main(String[] args) {
+        String name = "Abi";
+        int age = 30;
+        String print = name + age;
+        System.out.println( "This is your name and age "+ print);
+
+    }
+}
