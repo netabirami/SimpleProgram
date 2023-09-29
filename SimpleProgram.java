3afbeff8f32159c8957664b487485853c5ce9e45
netabirami <net.abirami@gmail.com>
2023-09-29 11:50:54 +0200

    This the ci/cd process

---
+++ SimpleProgram.java
@@ -0,0 +1,10 @@
+public class SimpleProgram {
+    public static void main(String[] args) {
+        String name = "Abi";
+        int age = 30;
+       // String print = name + age;
+        System.out.println( "This is your name and age = "+ name+" - "+age);
+
+    }
+}
+
